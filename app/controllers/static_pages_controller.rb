f6f6210b00a021fc8fd83a824e96eaf5a112a682
watertechplumbing2015 <watertechplumbing2015@gmail.com>
2015-08-22 16:28:50 +0530

    first commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,19 @@
+class StaticPagesController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def partners
+  end
+
+  def contact
+  end
+
+  def support
+  end
+
+  def purchase
+  end
+end
